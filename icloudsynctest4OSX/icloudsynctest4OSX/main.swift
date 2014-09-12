205738c6ae52da3c062d210a18bd763ec2c62829
Claude Montpetit <c@montpetit.net>
2014-09-12 19:34:34 -0400

    initial commit

---
+++ icloudsynctest4OSX/icloudsynctest4OSX/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  icloudsynctest4OSX
+//
+//  Created by Claude Montpetit on 2014-09-11.
+//  Copyright (c) 2014 org.montpetit. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ icloudsynctest4iOS/icloudsynctest4iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  icloudsynctest4iOS
+//
+//  Created by Claude Montpetit on 2014-09-11.
+//  Copyright (c) 2014 org.montpetit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
